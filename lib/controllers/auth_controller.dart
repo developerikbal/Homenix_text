3ab0a580f54a3270107f261c05d70c40dc94345c
developerikbal <frontendwebdeveloperikbal@gmail.com>
2025-06-30 17:26:57 +0530

    Update auth_controller.dart

--- lib/controllers/auth_controller.dart
+++ lib/controllers/auth_controller.dart
@@ -1,5 +1,6 @@
+// lib/controllers/auth_controller.dart
+
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class AuthController extends GetxController {
